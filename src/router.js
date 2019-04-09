fb9740eedec5a8a2400e564d78d5905c229f067a
AlfredPC <xjxtju@163.com>
2019-04-09 10:30:22 +0800

    eslint

--- src/router.js
+++ src/router.js
@@ -1,10 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import HomePage from './views/HomePage.vue'
+import Archive from './views/Archive.vue'
+import ContentPage from './views/ContentPage.vue'
+import Login from './views/Login.vue'
+import AdminEdit from './views/AdminEdit.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-   
+    {
+        path:'/',
+        redirect:'/home'
+    }, 
+    {
+        name:'home',
+        path:'/home',
+        component:HomePage
+    },
+    {
+        name:'archive',
+        path:'/archive',
+        component:Archive
+    },
+    {
+        name:'content',
+        path:'/tempC',
+        component:ContentPage
+    },
+    {
+        name:'login',
+        path:'/login',
+        component:Login
+    },
+    {
+        name:'admin',
+        path:'/admin',
+        component:AdminEdit
+    }
   ]
 })
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="home-page">
-      
+      <the-header></the-header>
+      222222
   </div>
 </template>
 <script>
+import TheHeader from '../components/TheHeader'
 export default {
-    
+    components:{
+        TheHeader
+    }
 }
 </script>
 
