1822df6ea1f7a8daaf810f8b4497046f126dcfb2
AlfredPC <xjxtju@163.com>
2019-04-09 10:36:08 +0800

    home sturture

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="the-header">
-      xcew
+      
   </div>
 </template>
 <script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="home-page">
       <the-header></the-header>
-      222222
+      <home-swiper></home-swiper>
+      <div class="home-context">
+          <ul class="ctx-card"></ul>
+          <home-side-bar></home-side-bar>
+      </div>
   </div>
 </template>
 <script>
 import TheHeader from '../components/TheHeader'
+import HomeSwiper from '../components/HomeSwiper'
+import HomeCard from '../components/HomeCard'
+import HomeSideBar from '../components/HomeSideBar'
+import TheFooter from '../components/TheFooter'
 export default {
     components:{
-        TheHeader
+        TheHeader,HomeSwiper,HomeCard,HomeSideBar,TheFooter
     }
 }
 </script>
