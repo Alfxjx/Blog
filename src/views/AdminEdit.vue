704ac2d9155f2919931cf1f77179252c443b84b8
AlfredPC <xjxtju@163.com>
2019-06-20 21:08:24 +0800

    部分引入table

--- src/views/AdminEdit.vue
+++ src/views/AdminEdit.vue
@@ -2,16 +2,61 @@
   <div id="admin-edit">
     <h1 class="admin-placeholder">1</h1>
     <div class="admin-main">
-      <div ref="charts" :style="{width: '300px', height: '300px'}"></div>
+      <div class="chart-admin">
+        <div ref="charts" :style="{width: '300px', height: '300px'}"></div>
+      </div>
+      <div class="crud-wrapper">
+        <el-table
+          :data="tableData"
+          stripe
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
     <the-footer></the-footer>
   </div>
 </template>
 <script>
   import TheFooter from '../components/TheFooter'
+  import { Table, TableColumn } from 'element-ui'
 
   export default {
     name: 'adminEdit',
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
     mounted() {
       this._drawLine()
     },
@@ -42,7 +87,9 @@
       }
     },
     components: {
-      TheFooter
+      TheFooter,
+      elTable: Table,
+      elTableColumn: TableColumn
     }
   }
 </script>
